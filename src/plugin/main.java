4231f22978583587740bf4c57040635c2e19afa3
BobEsponjeYT <75587701+BobEsponjeYT@users.noreply.github.com>
2021-07-02 21:03:25 -0300

    Update main.java

--- src/plugin/main.java
+++ src/plugin/main.java
@@ -1,1 +1,17 @@
+package src.plugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin{
+	PluginDescriptionFile pdffile = getDescription();
+	public String name = pdffile.getName();
+	public String version = pdffile.getVersion();
+	public String prefix = ChatColor.RED+"[Lobby]";
+	
+	public void onEnable() {
+		Bukkit.getConsoleSender().sendMessage(prefix+ChatColor.GREEN+" Plugin running in Version: "+ChatColor.AQUA+version);
+	}
+}
