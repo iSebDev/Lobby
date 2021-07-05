9fa955349e9ac641635e0ae5ea33a74961d85921
EpicX14 <51869289+EpicX14@users.noreply.github.com>
2021-07-05 16:07:39 -0500

    Update main.java

--- src/plugin/main.java
+++ src/plugin/main.java
@@ -6,12 +6,65 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin{
-	PluginDescriptionFile pdffile = getDescription();
-	public String name = pdffile.getName();
-	public String version = pdffile.getVersion();
-	public String prefix = ChatColor.RED+"[Lobby]";
+
+    private FileConfiguration messages = null;
+    private File messagesFile = null;
+
+    PluginDescriptionFile pdffile = getDescription();
+    public String name = pdffile.getName();
+    public String version = pdffile.getVersion();
+    public String prefix = ChatColor.RED+"[Lobby]";
 	
-	public void onEnable() {
-		Bukkit.getConsoleSender().sendMessage(prefix+ChatColor.GREEN+" Plugin running in Version: "+ChatColor.AQUA+version);
+    public void onEnable() {
+		registerMessages();
+        Bukkit.getConsoleSender().sendMessage(prefix+ChatColor.GREEN+" Plugin running in Version: "+ChatColor.AQUA+version);
+    }
+
+    //File Messages.yml
+	public FileConfiguration getMessages() {
+		if(messages == null) {
+			reloadMessages();
+		}
+		return messages;	
 	}
+	
+	public void reloadMessages() {
+		if(messages == null) {
+			messagesFile = new File(getDataFolder(),"messages.yml");
+		}
+		messages = YamlConfiguration.loadConfiguration(messagesFile);
+		Reader defConfigStream;
+		try {
+			
+			defConfigStream = new InputStreamReader(this.getResource("messages.yml"),"UTF8");
+			if(defConfigStream != null) {
+				YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+				messages.setDefaults(defConfig);
+			}
+		}catch(UnsupportedEncodingException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveMessages() {
+		try {
+			messages.save(messagesFile);
+		}catch(IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void registerMessages() {
+		messagesFile = new File(this.getDataFolder(),"messages.yml");
+		if(!messagesFile.exists()) {
+			this.getMessages().options().copyDefaults(true);
+			saveMessages();
+		}
+		
+		
+	}
+    //
 }
