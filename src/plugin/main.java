a5f6556d99a436385b2a26ff89342854e1f200fd
EpicX14 <51869289+EpicX14@users.noreply.github.com>
2021-07-05 16:17:13 -0500

    Update main.java

--- src/plugin/main.java
+++ src/plugin/main.java
@@ -1,70 +1,79 @@
 package src.plugin;
-
+ 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+ 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
-
+ 
 public class main extends JavaPlugin{
-
+ 
     private FileConfiguration messages = null;
     private File messagesFile = null;
-
+    FileConfiguration messgaesFileReal = getMessages();
+ 
     PluginDescriptionFile pdffile = getDescription();
     public String name = pdffile.getName();
     public String version = pdffile.getVersion();
-    public String prefix = ChatColor.RED+"[Lobby]";
-	
+        String prefixPath = messgaesFileReal.getString("messages.prefix");
+        public String prefix = ChatColor.translateAlternateColorCodes('&', prefixPath);
+ 
     public void onEnable() {
-		registerMessages();
         Bukkit.getConsoleSender().sendMessage(prefix+ChatColor.GREEN+" Plugin running in Version: "+ChatColor.AQUA+version);
+        yamlRegister();
     }
-
-    //File Messages.yml
-	public FileConfiguration getMessages() {
-		if(messages == null) {
-			reloadMessages();
-		}
-		return messages;	
-	}
-	
-	public void reloadMessages() {
-		if(messages == null) {
-			messagesFile = new File(getDataFolder(),"messages.yml");
-		}
-		messages = YamlConfiguration.loadConfiguration(messagesFile);
-		Reader defConfigStream;
-		try {
-			
-			defConfigStream = new InputStreamReader(this.getResource("messages.yml"),"UTF8");
-			if(defConfigStream != null) {
-				YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
-				messages.setDefaults(defConfig);
-			}
-		}catch(UnsupportedEncodingException e) {
-			
-			e.printStackTrace();
-		}
-	}
-	
-	public void saveMessages() {
-		try {
-			messages.save(messagesFile);
-		}catch(IOException e) {
-			
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public void registerMessages() {
-		messagesFile = new File(this.getDataFolder(),"messages.yml");
-		if(!messagesFile.exists()) {
-			this.getMessages().options().copyDefaults(true);
-			saveMessages();
-		}
-		
-		
-	}
+ 
+    public FileConfiguration getMessages() {
+        if(messages == null) {
+            reloadMessages();
+        }
+        return messages;    
+    }
+ 
+    public void reloadMessages() {
+        if(messages == null) {
+            messagesFile = new File(getDataFolder(),"messages.yml");
+        }
+        messages = YamlConfiguration.loadConfiguration(messagesFile);
+        Reader defConfigStream;
+        try {
+ 
+            defConfigStream = new InputStreamReader(this.getResource("messages.yml"),"UTF8");
+            if(defConfigStream != null) {
+                YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+                messages.setDefaults(defConfig);
+            }
+        }catch(UnsupportedEncodingException e) {
+ 
+            e.printStackTrace();
+        }
+    }
+ 
+    public void saveMessages() {
+        try {
+            messages.save(messagesFile);
+        }catch(IOException e) {
+ 
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+    public void yamlRegister() {
+        messagesFile = new File(this.getDataFolder(),"messages.yml");
+        if(!messagesFile.exists()) {
+            this.getMessages().options().copyDefaults(true);
+            saveMessages();
+        }
+ 
+ 
+    }
     //
 }
